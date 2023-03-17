53788aab5756debede8c6b4b0d17432fbadfb498
Sankalp Pandey <82685501+sanpdy@users.noreply.github.com>
2023-03-17 11:53:53 -0500

    Add files via upload
    An implementation of stacks and queues using the vector class in C++.

---
+++ Homework 4/Stack.h
@@ -0,0 +1,32 @@
+//
+// Created by Sankalp Pandey on 3/9/23.
+//
+
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Stack
+{
+public:
+    Stack();
+    Stack(const Stack& copy);
+    ~Stack();
+
+    void push(const int number);
+    int pop();
+    int top() const;
+
+    bool isFull() const;
+    bool isEmpty() const;
+    int getCount() const;
+    void print() const;
+
+private:
+    int count;
+    vector <int> data;
+};
+#endif //_STACK_H_
